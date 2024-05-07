c8535a0988b9931ab1fd03d527fd85b42c56475d
charlie <charliechen684@gmail.come>
2024-05-07 12:18:21 +1200

    commit 2

--- Homepage.py
+++ Homepage.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("hello world 2")
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Planning App")
+
+calender_label = ttk.Label(root, text="Calender")
+calender_label.grid(row=0, column=0)
+
+root.mainloop()
